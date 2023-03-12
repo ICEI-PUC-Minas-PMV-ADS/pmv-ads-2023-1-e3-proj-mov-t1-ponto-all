c4087a16acef1ef1f58d4a4ab1e4b41452c9ec51
DanielMarkline <592075@sga.pucminas.br>
2023-03-12 15:57:22 -0300

    Teste -React Native

---
+++ src/pontoall/src/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {
+
+return (
+
+  <View style={{flex:1, alignItems:'center', justifyContent:'center'}}> 
+
+      <Text> Ponto ALL </Text>
+    
+    </View>
+
+
+);
+
+}
+
+export default HomePage;
